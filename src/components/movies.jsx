b75e5a5be0a2a7943a4956e92fd8f8c658d18413
Daniel Stephenson <daniel@giffgaff.co.uk>
2019-06-28 11:42:39 +0100

    Show all genres

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -10,11 +10,12 @@ class Movies extends Component {
   state = { movies: [], genres: [], pageSize: 4, currentPage: 1 };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: 'All Genres' }, ...getGenres()];
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleGenreSelect = genre => {
-    this.setState({ selectedGenre: genre });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   handleDelete = movie => {
@@ -45,9 +46,10 @@ class Movies extends Component {
 
     if (count === 0) return <p>There are no movies in the database!</p>;
 
-    const filtered = selectedGenre
-      ? allMovies.filter(m => m.genre._id === selectedGenre._id)
-      : allMovies;
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+        : allMovies;
 
     const movies = paginate(filtered, currentPage, pageSize);
 
