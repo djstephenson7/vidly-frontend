172fcf57c0c03f0f981c588a16c10955682260b2
Daniel Stephenson <daniel@giffgaff.co.uk>
2019-07-02 10:49:11 +0100

    Error messages without clicking save

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -22,22 +22,34 @@ class LoginForm extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-
     const errors = this.validate();
-    console.log(errors);
 
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === 'username') {
+      if (value.trim() === '') return 'Username is required.';
+    }
+    if (name === 'password') {
+      if (value.trim() === '') return 'Password is required.';
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -47,12 +59,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
