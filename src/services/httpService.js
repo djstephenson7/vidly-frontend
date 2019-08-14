2a21ddc0c0cbcf841b5a080b1500e1c5e457b484
Daniel Stephenson <daniel@giffgaff.co.uk>
2019-08-14 09:57:23 +0100

    Env vars

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import logger from "./logService";
 
+axios.defaults.baseURL = process.env.REACT_API_APP_URL;
+
 axios.interceptors.response.use(null, error => {
   const expectedError =
     error.response &&
